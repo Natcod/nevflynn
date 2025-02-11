aac8e2710e9140a517f77b55b0abfd738b040b92
Natcod <natnaelshetaye1@gmail.com>
2025-02-11 16:24:23 +0300

    Remove duplicate "All" button from Navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,10 +4,10 @@ import "./Navigation.css"; // Import styles (optional)
 const Navigation = ({ setActiveSection }) => {
   return (
     <nav className="navigation">
+      <button onClick={() => setActiveSection("all")}>All</button>
       <button onClick={() => setActiveSection("about")}>About</button>
       <button onClick={() => setActiveSection("projects")}>Projects</button>
       <button onClick={() => setActiveSection("media")}>Media</button>
-      <button onClick={() => setActiveSection("all")}>All</button>
     </nav>
   );
 };
