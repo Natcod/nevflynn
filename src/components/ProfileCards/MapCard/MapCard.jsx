3d64b7975e7a81a4b56192cd61116433f1cfe2f5
Natcod <natnaelshetaye1@gmail.com>
2025-02-11 13:43:28 +0300

    Add MapCard component with zoom controls and responsive layout

---
+++ src/components/ProfileCards/MapCard/MapCard.jsx
@@ -0,0 +1,70 @@
+import React, { useRef } from "react";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css"; // Leaflet CSS
+import "./MapCard.css";
+
+// Avatar marker icon
+const avatarIcon = new L.Icon({
+  iconUrl: "/avatar.png", // Path to your avatar image
+  iconSize: [40, 40], // Size of the icon
+  iconAnchor: [20, 40], // Point of the icon that corresponds to the marker's location
+});
+
+const MapCard = () => {
+  const mapRef = useRef(null); // Ref to access the map instance
+  const mapCenter = [53.3498, -6.2603]; // Center on Dublin, Ireland
+
+  // Function to handle zoom in
+  const handleZoomIn = () => {
+    if (mapRef.current) {
+      const currentZoom = mapRef.current.getZoom();
+      mapRef.current.setZoom(currentZoom + 1);
+      mapRef.current.setView(mapCenter, currentZoom + 1); // Lock the center
+    }
+  };
+
+  // Function to handle zoom out
+  const handleZoomOut = () => {
+    if (mapRef.current) {
+      const currentZoom = mapRef.current.getZoom();
+      mapRef.current.setZoom(currentZoom - 1);
+      mapRef.current.setView(mapCenter, currentZoom - 1); // Lock the center
+    }
+  };
+
+  return (
+    <div className="map-card">
+      {/* Leaflet Map */}
+      <MapContainer
+        center={mapCenter} // Center on Dublin, Ireland
+        zoom={12} // Initial zoom level
+        scrollWheelZoom={false} // Disable scroll zoom
+        dragging={false} // Disable dragging (panning)
+        whenCreated={(map) => (mapRef.current = map)} // Save the map instance
+        className="map-container"
+      >
+        {/* Tile Layer (OpenStreetMap) */}
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {/* Avatar Marker */}
+        <Marker position={mapCenter} icon={avatarIcon} />
+      </MapContainer>
+
+      {/* Zoom Controls */}
+      <div className="zoom-controls">
+        <button className="zoom-button zoom-out" onClick={handleZoomOut}>
+          -
+        </button>
+        <button className="zoom-button zoom-in" onClick={handleZoomIn}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MapCard;
--- src/components/ProfileCards/ProfileCards.jsx
+++ src/components/ProfileCards/ProfileCards.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import AvatarCard from "./AvatarCard/AvatarCard";
+import MapCard from "./MapCard/MapCard"; // Import the MapCard component
 import "./ProfileCards.css";
 
 const ProfileCards = () => {
   return (
-    <div className="profile-cards">
-      <h2>Profile Cards</h2>
-      <AvatarCard />
+    <div className="about-container">
+      {/* Avatar Card */}
+      <div className="avatar-card-container">
+        <AvatarCard />
+      </div>
+
+      {/* Map Card */}
+      <div className="map-card-container">
+        <MapCard />
+      </div>
     </div>
   );
 };
