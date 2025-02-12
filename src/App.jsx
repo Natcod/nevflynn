0ec981a8b55724ee139e07987b3b07ff16271924
Natcod <natnaelshetaye1@gmail.com>
2025-02-12 08:16:47 +0300

    Implement Dark Mode support with styling adjustments and integrate NightModeToggleCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import React, { useState } from "react";
-import Navigation from "./components/Navigation/Navigation";
-import ProfileCards from "./components/ProfileCards/ProfileCards";
-import ProjectsSection from "./components/ProjectsSection/ProjectsSection"; // Import ProjectsSection
-// import MediaSection from "./components/MediaSection/MediaSection"; // Import MediaSection
-import "./App.css";
+import React from 'react';
+import { DarkModeProvider } from './components/DarkModeContext/DarkModeContext';
+import Navigation from './components/Navigation/Navigation';
+import ProfileCards from './components/ProfileCards/ProfileCards';
+import ProjectsSection from './components/ProjectsSection/ProjectsSection';
+import MediaSection from './components/MediaSection/MediaSection';
+import NightModeSection from './components/NightModeToggleCard/NightModeToggleCard';
+import './App.css';
 
 const App = () => {
-  const [activeSection, setActiveSection] = useState("all");
+  const [activeSection, setActiveSection] = React.useState('all');
 
   return (
-    <div className="app">
-      <Navigation setActiveSection={setActiveSection} />
-      <div className="content">
-        {/* Render all sections when "All" is clicked */}
-        {activeSection === "all" && (
-          <>
-            <ProfileCards />
-            <ProjectsSection />
-            {/* <MediaSection /> */}
-          </>
-        )}
-
-        {/* Render individual sections based on activeSection */}
-        {activeSection === "about" && <ProfileCards />}
-        {activeSection === "projects" && <ProjectsSection />}
-        {/* {activeSection === "media" && <MediaSection />} */}
+    <DarkModeProvider>
+      <div className="app">
+        <Navigation setActiveSection={setActiveSection} />
+        <div className="content">
+          {/* Render all sections when "All" is clicked */}
+          {activeSection === 'all' && (
+            <>
+              <ProfileCards />
+              <ProjectsSection />
+              <MediaSection />
+            </>
+          )}
+          {/* Render individual sections based on activeSection */}
+          {activeSection === 'about' && <ProfileCards />}
+          {activeSection === 'projects' && <ProjectsSection />}
+          {activeSection === 'media' && <MediaSection />}
+        </div>
+        <NightModeSection />
       </div>
-    </div>
+    </DarkModeProvider>
   );
 };
 
