96969ebb64a106d7257a08b58332293faeb28438
Natcod <natnaelshetaye1@gmail.com>
2025-02-15 14:07:44 +0300

    Refactor ProjectsSection to dynamically render card components and update ProjectCard styles for improved layout

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { SortableContext, useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import ProjectCard from './ProjectCard/ProjectCard';
 import './ProjectsSection.css';
+
+// Import ProjectCard and other card components
+import ProjectCard from '../ProjectsSection/ProjectCard/ProjectCard';
+import AvatarCard from '../ProfileCards/AvatarCard/AvatarCard';
+import MapCard from '../ProfileCards/MapCard/MapCard';
+import TwitterCard from '../ProfileCards/TwitterCard/TwitterCard';
+import SpotifyCard from '../MediaSection/SpotifyCard/SpotifyCard';
+import SubscribeCard from '../MediaSection/SubscribeCard/SubscribeCard';
+import ReadMoreCard from '../MediaSection/ReadMoreCard/ReadMoreCard';
+import NightModeToggleCard from '../NightModeToggleCard/NightModeToggleCard';
+
+// Mapping of card types to their respective components
+const cardTypeToComponent = {
+  ProjectCard: ProjectCard,
+  AvatarCard: AvatarCard,
+  MapCard: MapCard,
+  TwitterCard: TwitterCard,
+  SpotifyCard: SpotifyCard,
+  SubscribeCard: SubscribeCard,
+  ReadMoreCard: ReadMoreCard,
+  NightModeToggleCard: NightModeToggleCard,
+  
+  // Add more card types as needed
+};
 
 const ProjectsSection = ({ cards }) => {
   return (
@@ -10,7 +33,12 @@ const ProjectsSection = ({ cards }) => {
       <SortableContext items={cards.map((card) => card.id)}>
         {cards.map((card) => (
           <SortableItem key={card.id} id={card.id}>
-            <ProjectCard {...card} />
+            {/* Dynamically render the correct component based on card type */}
+            {cardTypeToComponent[card.type] ? (
+              React.createElement(cardTypeToComponent[card.type], card)
+            ) : (
+              <div>Unknown Card Type</div>
+            )}
           </SortableItem>
         ))}
       </SortableContext>
@@ -24,7 +52,6 @@ const SortableItem = ({ id, children }) => {
     transform: CSS.Transform.toString(transform),
     transition,
   };
-
   return (
     <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
       {children}
