0fafc4e64f1bdcb4dfb2367359acef6be62cb322
Natcod <natnaelshetaye1@gmail.com>
2025-02-19 14:10:17 +0300

    Add react-grid-layout-next dependency and update postcss and tailwindcss versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    content: [
-      './src/**/*.{html,js}',
-    ],
-    theme: {
-      extend: {},
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'bento': 'repeat(auto-fit, minmax(200px, 1fr))',
+      },
     },
-    plugins: [],
-  }
+  },
+};
